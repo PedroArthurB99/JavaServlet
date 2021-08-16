98f53e8c1cb0234b02aaef8a5649f4493be396e6
Pedro Arthur Barbosa Carvalho <pedro.barbosa@zup.com.br>
2021-08-16 14:23:04 -0300

    ultimo commit

--- src/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ src/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/listaEmpresas")
+@WebServlet(urlPatterns="/listaEmpresas", loadOnStartup=1)
 public class ListaEmpresasServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
